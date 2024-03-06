ee8feb1282c0707d983d1240a02d235763344dc1
Selmir Kuduzovic <102289679+S3MA20@users.noreply.github.com>
2024-03-06 20:32:28 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+var app = angular.module('myApp',[]);
+
+
+
+app.controller('mainController', function ($scope){
+    $scope.searchBy = '';
+    $scope.languages = [
+        {name:'C#', likes: 0, dislikes: 0},
+        {name:'Java', likes: 0, dislikes: 0},
+        {name:'JavaScript', likes: 0, dislikes: 0},
+        {name:'PHP', likes: 0, dislikes: 0},
+    ];
+    $scope.limitRows = $scope.languages.length;
+    $scope.newLanguage = '';
+
+    $scope.likeLanguage = function(lang){
+        lang.likes++;
+    }
+
+    $scope.dislikeLanguage = function(lang){
+        lang.dislikes++;
+    }
+
+    $scope.deleteLanguage = function(index){
+        $scopelanguages.splice(index,1);
+    }
+
+    $scope.saveLang = function(){
+        $scope.languages.push({name: $scope.newLanguage, likes: 0, dislike: 0})
+        $scope.limitRows = $scope.languages.length;
+        $scope.newLanguage = '';
+    }
+});
